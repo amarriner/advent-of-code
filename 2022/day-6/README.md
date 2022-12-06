6a410caf33cf1f37fa40717b5b49da4d01bebb9c
Aaron Marriner <aaron.marriner@ellucian.com>
2022-12-06 08:21:08 -0500

    Day 6

---
+++ 2022/day-6/README.md
@@ -0,0 +1,2 @@
+## [Advent of Code 2022 Day 6](https://adventofcode.com/2022/day/6)
+
---
+++ 2022/day-6/part-2.py
@@ -0,0 +1,17 @@
+import logging
+import sys
+
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+MARKER_SIZE = 14
+
+with open('input.txt') as f:
+
+    buffer = f.read()
+
+for index, char in enumerate(buffer[:len(buffer) - MARKER_SIZE]):
+    marker = buffer[index:index + MARKER_SIZE]
+    if len(set(marker)) == MARKER_SIZE:
+        logging.info(f'Found marker {marker} at index {index}, needed to process {index + MARKER_SIZE} characters first')
+        break
