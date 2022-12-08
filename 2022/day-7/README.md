2063cb3f4ea307d8cd0cdbc175fbb73772e89057
Aaron Marriner <aaron.marriner@ellucian.com>
2022-12-08 09:18:20 -0500

    Day 7

---
+++ 2022/day-7/README.md
@@ -0,0 +1,2 @@
+## [Advent of Code 2022 Day 7](https://adventofcode.com/2022/day/7)
+
---
+++ 2022/day-7/part-1.py
@@ -0,0 +1,68 @@
+import logging
+import re
+import sys
+
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+CD_PATTERN = re.compile(r'^\$ cd (.*)$')
+LS_PATTERN = re.compile(r'^\$ ls.*$')
+THRESHOLD = 100000
+
+
+def cd(structure, cwd, directory):
+
+    logging.debug(f'Changing dir from {cwd} to {directory}')
+
+    if directory == "..":
+        return "/".join(cwd.split("/")[:-1])
+
+    elif directory == "/":
+        return directory
+
+    else:
+        if cwd == '/':
+            newdir = f'/{directory}'
+        else:
+            newdir = f'{cwd}/{directory}'
+
+        if newdir not in structure:
+            structure[newdir] = {}
+
+        return newdir
+
+
+def get_size(structure, directory):
+
+    size = 0
+    for dir in [dir for dir in structure if dir.startswith(directory)]:
+        size += sum([structure[dir][file] for file in structure[dir]])
+
+    return size
+
+
+def ls(structure, cwd, contents):
+
+    size, name = contents.split(" ")
+    if size != "dir":
+        structure[cwd][name] = int(size)
+
+
+if __name__ == '__main__':
+
+    with open('input.txt') as f:
+        lines = [line for line in f.read().split("\n") if line != ""]
+
+    structure = {'/': {}}
+    cwd = "/"
+    for line in lines:
+
+        logging.debug(line)
+
+        if CD_PATTERN.match(line):
+            cwd = cd(structure, cwd, CD_PATTERN.match(line).groups()[0])
+
+        elif not LS_PATTERN.match(line):
+            ls(structure, cwd, line)
+
+    logging.info(f"Total size of directories < {THRESHOLD}: {sum([get_size(structure, dir) for dir in structure if get_size(structure, dir) < THRESHOLD])}")
